9fce55c2ef08f9b47e11bfe6e9a224abba72fcd2
Erick Rocha <erickfr1011@gmail.com>
2023-11-07 12:06:50 -0300

    Criar comandos de definicao de tabelas

--- create_database.sql
+++ create_database.sql
@@ -10,17 +10,52 @@ CREATE TABLE usuario(
   PRIMARY KEY(login)
 );
 
-CREATE TABLE produto(
-  id_produto int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY(id_produto)
+CREATE TABLE categoria(
+  nome_categoria varchar(50) NOT NULL,
+  PRIMARY KEY (nome_categoria)
 );
 
-CREATE TABLE avaliacao(
+CREATE TABLE produto(
+  id_produto int(11) NOT NULL AUTO_INCREMENT,
+  nome_categoria varchar(50) NOT NULL,
+  nome varchar(50) NOT NULL,
+  descricao varchar(200),
+  preco decimal(10,2) NOT NULL,
+  PRIMARY KEY(id_produto),
+  FOREIGN KEY (nome_categoria) REFERENCES categoria(nome_categoria),
+  UNIQUE(nome)
+);
+
+CREATE TABLE pedido(
+  id_pedido int(11) NOT NULL AUTO_INCREMENT,
+  status int(11) NOT NULL,
   login_usuario varchar(50) NOT NULL,
+  PRIMARY KEY(id_pedido),
+  FOREIGN KEY (login_usuario) REFERENCES usuario(login)
+);
+
+CREATE TABLE item_pedido(
+  id_pedido int(11) NOT NULL,
   id_produto int(11) NOT NULL,
-  nota int(1) NOT NULL,
-  comentario varchar(200),
-  PRIMARY KEY(login_usuario, id_produto),
-  FOREIGN KEY (login_usuario) REFERENCES usuario(login),
+  quantidade int(11) NOT NULL,
+  PRIMARY KEY(id_pedido, id_produto),
+  FOREIGN KEY (id_pedido) REFERENCES pedido(id_pedido),
+  FOREIGN KEY (id_produto) REFERENCES produto(id_produto)
+);
+
+CREATE TABLE pagamento(
+  codigo int(11) NOT NULL AUTO_INCREMENT,
+  tipo int(11) NOT NULL,
+  endereco varchar(200),
+  id_pedido int(11) NOT NULL,
+  PRIMARY KEY(codigo),
+  FOREIGN KEY (id_pedido) REFERENCES pedido(id_pedido)
+);
+
+CREATE TABLE informacao(
+  id_produto int(11) NOT NULL,
+  titulo varchar(50) NOT NULL,
+  corpo varchar(200) NOT NULL,
+  PRIMARY KEY(id_produto, titulo),
   FOREIGN KEY (id_produto) REFERENCES produto(id_produto)
 );
