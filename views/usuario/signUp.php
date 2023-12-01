305679c86fc63af9606f0fe7653a3b5cd7402555
Ana <analuisareiis@gmail.com>
2023-12-01 09:16:07 -0300

    Formulario do Sign Up

--- views/usuario/signUp.php
+++ views/usuario/signUp.php
@@ -1,29 +1,46 @@
-<div>
-    <form action="index.php" method="post">
-    <p align="center">
+
+<div class="conteiner-text-center">
+    <form class="row g-3 justify-content-center" action="index.php" method="post">
+    <div class="row" style="margin-top: 10px;">
+        <div class="col-md-3">
         <label for="login">Usuário:</label>
-        <input type="text" name="login">
-    </p>
+        <input type="text" class="form-control" name="login">
+        </div>
+    </div>
     
-    <p align="center">
+    <div class="row" style="margin-top: 10px;">
+      <div class="col-md-3">
         <label for="login">Nome:</label>
-        <input type="text" name="nome">
-    </p>
+        <input type="text" class="form-control" name="nome">
+      </div>
+    </div>
     
 
-    <p align="center">
+    <div class="row" style="margin-top: 10px;">
+      <div class="col-md-3">
         <label for="password">Senha:</label>
-        <input type="password" name="senha">
-    </p>
+        <input type="password" class="form-control" name="senha">
+      </div>
+    </div>
 
-    <p align="center">
+
+    <div class="row" style="margin-top: 10px;">
+      <div class="col-md-3">
         <label for="password">Senha:</label>
-        <input type="password" name="senha_confirma">
-    </p>
+        <input type="password" class="form-control" name="senha_confirma" aria-describedby="passwordHelpBlock">
+        <div class="col-auto">
+         <span id="passwordHelpInline" class="form-text">
+             Deve conter 4-20 caracteres.
+         </span>
+        </div>
+      </div>
+    </div>
     
-    <p align="center">
-        <button> sign up </button>
-    </p>
+    <div class="row" style="margin-top: 10px;">
+      <div class="col-md-3">
+        <button type="submit" class="btn btn-outline-primary" style="margin-top: 10px;"> sign up </button>
+      </div>
+    </div>
 
     <input type="hidden" name="class" value="Usuario"/> 
     <input type="hidden" name="action" value="authenticate_sign_up"/>
